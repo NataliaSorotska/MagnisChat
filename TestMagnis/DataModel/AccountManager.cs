e6e7a425a58426e5a1a9ea15777ff754b138a29d
Natalia Sorotska <natalia.sorotska@gmail.com>
2017-01-14 16:01:53 +0200

    Created AccountManager;

---
+++ TestMagnis/DataModel/AccountManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestMagnis.DataModel
+{
+    class AccountManager
+    {
+        public bool RegisterAccount(Account account)
+        {
+            using (ChatContext context = new ChatContext())
+            {
+                bool isExisted = context.Accounts.Any(a => a.Name == account.Name);
+                if (!isExisted)
+                {
+                    context.Accounts.Add(account);
+                    context.SaveChanges();
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+
+
+        public Account GetAccount(string name, string password)
+        {
+            using (var context = new ChatContext())
+            {
+                Account person = context.Accounts.FirstOrDefault(p => p.Name == name && p.Password == password);
+                return person;
+            }
+        }
+    }
+}
+
