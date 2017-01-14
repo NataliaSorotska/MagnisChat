7ed67190ae383273783c984fa20c6055bb6a772e
Natalia Sorotska <natalia.sorotska@gmail.com>
2017-01-14 15:59:54 +0200

    Create Message model;

---
+++ TestMagnis/DataModel/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMagnis.DataModel
+{
+    public class Message
+    {
+        [Key]
+        public int MessageId { get; set; }
+
+        [Required]
+        public string MessageText { get; set; }
+
+        [Required]
+        public DateTime MessageDate { get; set; }
+
+        [Required]
+        public int AccountId { get; set; }
+    }
+}
