10de76c87c0196b5e3453cd2c3f136fec3c9adee
Natalia Sorotska <natalia.sorotska@gmail.com>
2017-01-14 19:51:28 +0200

    Added Server class;

---
+++ TestMagnis/Server.cs
@@ -0,0 +1,51 @@
+﻿using SuperSocket.SocketBase;
+using SuperWebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMagnis
+{
+    class Server
+    {
+        public WebSocketSession Session;
+        public WebSocketServer webserver;
+        public void Setup()
+        {
+            webserver = new WebSocketServer();
+            bool setupresult = webserver.Setup(2017);
+            if (!setupresult) { return; }
+            webserver.NewSessionConnected += OnConectedHandler;
+            webserver.SessionClosed += OnSessionClosedHandler;
+            webserver.NewMessageReceived += OnNewMessageReceivedHandler;
+            
+        }
+        public void Start()
+        {
+            webserver.Start();
+
+        }
+       private void OnConectedHandler(WebSocketSession session)
+        {
+            Console.WriteLine("session conect");
+            Session = session;
+           this.SendMessage();
+
+        }
+        private void OnSessionClosedHandler(WebSocketSession session, CloseReason reason)
+        {
+            Console.WriteLine("Session Close");
+        }
+        private void OnNewMessageReceivedHandler(WebSocketSession session, string message)
+        {
+            Console.WriteLine("Message Receive");
+            Console.WriteLine(message);
+        }
+        public void SendMessage()
+        {
+            Session.Send("HI");
+        }
+    }
+}
