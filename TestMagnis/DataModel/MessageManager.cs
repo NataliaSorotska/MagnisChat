1aa1ffc20bd9792c29919772af4a7c02b65c635c
Natalia Sorotska <natalia.sorotska@gmail.com>
2017-01-14 16:02:22 +0200

    Added MessageManager;

---
+++ TestMagnis/DataModel/MessageManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMagnis.DataModel
+
+{
+    class MessageManager
+    {
+        public void SaveMessage(Message message)
+        {
+
+            using (ChatContext context = new ChatContext())
+            {
+                context.Messages.Add(message);
+                context.SaveChanges();
+            }
+        }
+        public IEnumerable<Message> GetLastMessages(int count)
+        {
+            using (ChatContext context = new ChatContext())
+            {
+                Message[] lastMessages = context.Messages
+                    .OrderByDescending(m => m.MessageDate)
+                    .Take(count).
+                    ToArray();
+                return lastMessages;
+            }
+        }
+    }
+}
